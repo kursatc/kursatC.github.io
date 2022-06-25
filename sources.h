a4e2d47ac31ff20067e8f90af669ded5c7ca2438
Asina-Chie-Shih-Shuai <63533962+kursatc@users.noreply.github.com>
2022-06-25 11:51:25 +0300

    Add files via upload

---
+++ sources.h
@@ -0,0 +1,7 @@
+Sources :
+https://stackoverflow.com/questions/22492498/css-resize-zoom-in-effect-on-image-while-keeping-dimensions
+http://jsfiddle.net/qemgt1nz/1/
+https://stackoverflow.com/questions/6169666/how-to-resize-an-image-to-fit-in-the-browser-window
+https://stackoverflow.com/questions/11243075/css-scale-height-to-match-width-possibly-with-a-formfactor
+Viewport Width (vw)
+Viewport Height (vh)
